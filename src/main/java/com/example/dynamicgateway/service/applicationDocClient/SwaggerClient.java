2f4f6ddb07815f039825319fd0716f21f662bf99
Sergey <nad.chel@mail.ru>
2024-02-02 02:31:16 +0300

    handled 404 in SwaggerClient

--- src/main/java/com/example/dynamicgateway/service/applicationDocClient/SwaggerClient.java
+++ src/main/java/com/example/dynamicgateway/service/applicationDocClient/SwaggerClient.java
@@ -4,13 +4,19 @@ import com.example.dynamicgateway.model.discoverableApplication.DiscoverableAppl
 import com.example.dynamicgateway.service.swaggerDocParser.SwaggerDocParser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+
+import java.text.MessageFormat;
+import java.util.function.Function;
 
 /**
  * An {@link ApplicationDocClient} implementation for finding Swagger (Open API) documentation exposed by {@link DiscoverableApplication}s
  */
 @Getter
+@Slf4j
 public class SwaggerClient implements ApplicationDocClient<SwaggerParseResult> {
     private final String scheme;
     private final String docPath;
@@ -31,6 +37,19 @@ public class SwaggerClient implements ApplicationDocClient<SwaggerParseResult> {
                 .uri(scheme + application.getName() + docPath)
                 .retrieve()
                 .bodyToMono(String.class)
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        handle404NotFound(application))
                 .map(parser::parse);
+    }
+
+    private Function<WebClientResponseException.NotFound, Mono<? extends String>> handle404NotFound(DiscoverableApplication application) {
+        return ex -> {
+            log.warn(MessageFormat.format("""
+                    Could not find doc for {0}. If it''s not expected, make sure \
+                    {0} exposes its API and this SwaggerClient is configured \
+                    properly (e.g. docPath is correct) \
+                    """, application.getName()));
+            return Mono.empty();
+        };
     }
 }
